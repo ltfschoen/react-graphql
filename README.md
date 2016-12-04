a9dda8bc36abd2a90feee389d767e9a56c151609
Luke Schoen <ltfschoen@gmail.com>
2016-12-04 22:54:21 +1100

    Mounted GraphQL Schema endpoint with Express using GraphiQL Type System

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 * `npm install`
 * `npm start` and go to http://localhost:3000
 * Refresh page to show code changes
+* Go to GraphQL Server endpoint
+    * Query example http://localhost:3000/graphql?query={counter,message,dataArray,dataArrayOfObjects{counter},dataArrayOfLinkObjects{_id,url}} for JSON response
+    * Mutation example http://localhost:3000/graphql?mutation{incrementCounter} run multiple times to increment
+
+## TODO 
+* [ ] Setup Mongo instead of Mock API (shown in course)
 
 ## Credits 
 * Built based upon Pluralsight course Building Data-driven React Apps with Relay, GraphQL and Flux.
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import linkApi from './js/api/mockLinkApi';
 
+// GraphQL helper package for Express to mount the GraphQL Schema
+import schema from './js/data/schema';
+import GraphQLHTTP from 'express-graphql';
+
 const port = 3000;
 let app = express();
 app.use(express.static('public'));
+
+// Mount GraphQL Endpoint for Express server using GraphQLHTTP helper as callback takes GraphQL Schema as arg
+app.use('/graphql', GraphQLHTTP({
+  schema: schema,
+  graphiql: true // GraphiQL with built-in autocomplete for our GraphQL server
+}));
 
 app.listen(port, () => {
   console.log('Listening on port 3000');
